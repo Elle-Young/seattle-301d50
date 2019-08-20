376c64bb0cd20b8524c65e5881923e9fcb806894
elle <missanyayoung@outlook.com>
2019-08-20 11:50:27 -0700

    weather rendering to server

--- class-06/city-explorer-demo/server.js
+++ class-06/city-explorer-demo/server.js
@@ -10,6 +10,8 @@ app.use(cors());
 const PORT = process.env.PORT;
 // const PORT = 3007;
 
+//Location 
+
 app.get('/location', (request, response) => {
   try{
     const geoData = require('./data/geo.json');
@@ -27,13 +29,41 @@ app.get('/location', (request, response) => {
     longitude: lng
   } 
   response.send(formattedData);
+  
+}
+catch(error){
+  console.error(error);
+  response.send(error.message);
+}
+});
 
+//Weather
+app.get('/weather', (request, response)=>{
+  try{
+    const darkskyData = require('./data/darksky.json');
+    const time = darkskyData.currently.time;
+    const weather = darkskyData.daily.summary;
+
+    const formattedData ={
+      // search_query: searchQuery, 
+      // formattedQuery: formattedQuery,
+      current_time: time, 
+      current_forcast: weather
+    }
+    response.send(formattedData)
   } catch(error){
     console.error(error);
     response.send(error.message);
   }
+});
+//     const formattedData =
+//   }
+// })
+//   response.send(formattedData);
+  
+// }
+// }
 
-})
 
 //new constructor function that takes in time and forcast, searches 'this.search_"data key"' for time/searches 'this.serach_"data key"' for forcast, end function
 //get data from file
