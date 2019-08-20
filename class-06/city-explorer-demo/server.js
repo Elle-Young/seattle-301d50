bbe3550041630a459a59df40f6ac088d1e3881f8
elle <missanyayoung@outlook.com>
2019-08-20 16:28:15 -0700

    test

--- class-06/city-explorer-demo/server.js
+++ class-06/city-explorer-demo/server.js
@@ -12,28 +12,36 @@ const PORT = process.env.PORT;
 
 //Location 
 
+function Location(query, format, lat, lng){
+  this.search_query=query;
+  this.formatted_query=format;
+  this.latitude=lat;
+  this.longitude=lng;
+}
+
 app.get('/location', (request, response) => {
   try{
     const geoData = require('./data/geo.json');
 
-  const searchQuery =  request.query.data;
-  const formattedQuery = geoData.results[0].formatted_address;
+    const searchQuery =  request.query.data;
+    const specificGeoData =geoData.results[0];
+    const formattedQuery =specificGeoData.formatted_address;
 
-  const lat = geoData.results[0].geometry.location.lat;
-  const lng = geoData.results[0].geometry.location.lng;
+    const lat =specificGeoData.geometry.location.lat;
+    const lng =specificGeoData.geometry.location.lng;
 
-  const formattedData = {
-    search_query: searchQuery,
-    formatted_query: formattedQuery,
-    latitude: lat,
-    longitude: lng
+    const formattedData = {
+      search_query: searchQuery,
+      formatted_query: formattedQuery,
+      latitude: lat,
+      longitude: lng
   } 
   response.send(formattedData);
   
 }
-catch(error){
-  console.error(error);
-  response.send(error.message);
+catch(e){
+  console.error(e);
+  response.status(500).send(e.message);
 }
 });
 
@@ -50,22 +58,31 @@ app.get('/weather', (request, response)=>{
     let arr = [];
     const darkskyData = require('./data/darksky.json');
     const time = darkskyData.currently.time;
-    const weather = darkskyData.daily.summary;
+    const weather = darkskyData.daily.data;
+
+    const formattedDays = weather.map(day => new Day(day.summary, day.time));
     
     let bananas = new Weather(time, weather);
+
+    function Day (summary, time){
+      this.forcast =summary;
+      this.time = (time *1000).toDateString();
+      
+    
   
-    response.send(bananas)
+    // response.send(bananas)
     //iterate over darkskydata 7 times and create new weather object (as part of the constructor) push object to a new array and then return new array
 
     
     let timeData= darkskyData.daily.data[0];
     response.send(timeData);
+  };
 
-
-  } catch(error){
-    console.error(error);
-    response.send(error.message);
-  }
+}
+    catch(e){
+      console.error(e);
+      response.status(500).send(e.message);
+    }
 });
 
 
